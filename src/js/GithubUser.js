689988254105e11bf314f10ebee9502d65d5b18b
trentspi <tspice23@att.net>
2017-05-30 16:27:35 -0400

    added user styles, fetch new props when entering another user

--- src/js/GithubUser.js
+++ src/js/GithubUser.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 
+import '../css/GithubUser.css';
+
 class GithubUser extends Component {
   state = {
     user: {
@@ -8,19 +10,29 @@ class GithubUser extends Component {
       followers: '',
       following: '',
       location: '',
-      html_url: ''
+      html_url: '',
+      public_repos: ''
+
     }
   }
 
   constructor(props) {
     super(props);
-    this.fetchUserData();
+    this.fetchUserData(props);
   }
 
-  fetchUserData = () => {
-    fetch(`https://api.github.com/users/${this.props.match.params.username}`)
+  fetchUserData = (props) => {
+    fetch(`https://api.github.com/users/${props.match.params.username}`)
     .then(response => response.json())
     .then(user => this.setState({ user }))
+
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const locationChanged = (nextProps.location !== this.props.location)
+    if(locationChanged) {
+      this.fetchUserData(nextProps);
+    }
   }
 
   render() {
@@ -29,6 +41,7 @@ class GithubUser extends Component {
       <div className="github-user">
         <a href={user.html_url} target="_"><img src={user.avatar_url} alt="Github avatar" /></a>
         <h2>{user.login}</h2>
+        <h3>repositories: {user.public_repos}</h3>
         <h3>followers: {user.followers}</h3>
         <h3>following: {user.following}</h3>
         <h3>location: {user.location}</h3>
